a745e5be9b150ef485a0c0d907f692ccf4117132
Diogo <dapereira@inf.ufsm.br>
2025-10-24 15:20:59 -0300

    feat: models + serializers article obj

--- backend/article/models.py
+++ backend/article/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    source = models.CharField(max_length=255, default="Wikipedia")
+
+    def __str__(self):
+        return self.title
