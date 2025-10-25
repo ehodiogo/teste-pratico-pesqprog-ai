b426df94f649815db5414596fd89695c126601f6
Diogo <dapereira@inf.ufsm.br>
2025-10-24 22:20:06 -0300

    hotfix: criar agents usando só o crewai

--- backend/agent/urls.py
+++ backend/agent/urls.py
@@ -1,9 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import ArticleViewSet
+# from .views import ArticleViewSet
 
 app_name = "agent"
 
 router = DefaultRouter()
-router.register(r"articles", ArticleViewSet, basename="article")
+#router.register(r"articles", ArticleViewSet, basename="article")
 
 urlpatterns = router.urls
--- backend/article/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Article(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    source = models.CharField(max_length=255, default="Wikipedia")
-
-    def __str__(self):
-        return self.title
